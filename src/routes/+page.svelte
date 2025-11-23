f10a321b4c657067ff7dcb5df0dde2a2e41a93dd
seeni <seeni@lenova.com>
2025-11-23 12:04:15 +0530

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { data } from './shared.svelte';
+	import Test from './Test.svelte';
+	let numbers = $state([1, 2]);
+
+	function addNumber() {
+		numbers.push(2);
+	}
+	$inspect(numbers);
+</script>
+
+<p>{numbers}</p>
+
+<button onclick={addNumber}> Add a number </button>
+
+<p>From pareant {data.count}</p>
+<button onclick={() => data.count++}>add</button>
+
+<Test data={numbers} />
---
+++ src/routes/Test.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { data } from './shared.svelte';
+	const { data: tee } = $props();
+</script>
+
+<p>From child {data.count},, {tee}</p>
+<button onclick={() => data.count++}>add</button>
