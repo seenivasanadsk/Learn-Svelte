a5efa814cbf4a33227c32f0fe284b71463527b2f
seeni <seeni@lenova.com>
2025-11-25 07:54:52 +0530

    Updated use Directive in Notes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,30 @@
 <script>
-  const color = $state();
+  let color = $state('');
+  let shade = $state('');
+
+  function testUse(node) {
+    $effect(() => {
+      console.log('effect run', color, shade);
+
+      // Example of applying the color dynamically
+      node.style.backgroundColor = `color-${color}-${shade}`; // or any logic
+
+      // Return cleanup (destroy) function
+      return () => {
+        console.log('cleanup effect', color, shade);
+        node.style.backgroundColor = '';
+      };
+    });
+  }
 </script>
 
-<ul
-  style="color: {color ? 'blue' : 'green'}"
-  style:border="1px solid red"
-  style:padding="10px"
-  style:margin="10px"
->
-  <li>seeni</li>
-  <li>vasan</li>
-</ul>
+<div>
+  Your color: {color}-{shade}
+  <br />
+  <input type="text" bind:value={color} placeholder="red" />
+  <input type="text" bind:value={shade} placeholder="500" />
+
+  {#if color && shade}
+    <div class="w-5 h-5" use:testUse></div>
+  {/if}
+</div>
